b2fdbdea8a15c7c6e01f0a71699f98b877d1f39d
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-02-01 20:48:21 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'ComicsApp',
+  components: {
+    AppHeader
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <main></main>
+  <footer></footer>
+</template>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Generics */
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+body {
+  font-family: 'Encode Sans Condensed', sans-serif;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,117 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data: () => ({
+        links: [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+        ]
+    })
+}
+</script>
+
+<template>
+    <header>
+        <figure>
+            <img id="logo" src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="(link, i) in links" :key="i"><a :href="link.url" :class="{ active: link.current }">{{ link.text
+                }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 140px;
+}
+
+ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+}
+
+ul li {
+    margin: 1rem;
+    text-transform: uppercase;
+    font-weight: 600;
+}
+
+ul li a.active,
+ul li a:active {
+    position: relative;
+    color: #0282F9;
+}
+
+ul li a.active::after,
+ul li a:active::after {
+    content: '';
+    background-color: #0282F9;
+    height: 5px;
+    width: 100%;
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 75px;
+}
+
+nav {
+    display: flex;
+}
+</style>
